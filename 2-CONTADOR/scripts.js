93ee272d4d78e9e13965f1d2525519c476a8f5d3
Henriquesoto92 <henriquesoto92@gmail.com>
2022-04-28 21:13:52 -0300

    Contador feito em live 28.04

---
+++ 2-CONTADOR/scripts.js
@@ -0,0 +1,28 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const style = e.currentTarget.classList;
+        if (style.contains("decrease")) {
+            count--;
+        } else if (style.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    });
+});
